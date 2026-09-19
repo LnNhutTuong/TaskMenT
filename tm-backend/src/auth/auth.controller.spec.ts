eb67e3b9ec92b1090882ac27b3b384e1085f9874
LnNhutTuong <lnnhuttuong@gmail.com>
2026-09-20 00:46:18 +0700

    fix: auth spec

--- tm-backend/src/auth/auth.controller.spec.ts
+++ tm-backend/src/auth/auth.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { JwtAuthGuard } from './guards/jwt-auth.guard.js';
+import { AuthModuleOptions } from '@nestjs/passport';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -14,7 +16,11 @@ describe('AuthController', () => {
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [{ provide: AuthService, useValue: authService }],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: JwtAuthGuard, useValue: { canActivate: () => true } },
+        { provide: AuthModuleOptions, useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
